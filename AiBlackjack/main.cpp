f2839ab6960d194655a420479d2f576adcdeb757
Javohir Abdurazzakov <ja688@cornell.edu>
2024-01-09 13:42:11 -0500

    Initial Commit

---
+++ AiBlackjack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AiBlackjack
+//
+//  Created by Javohir Abdurazzakov on 1/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
